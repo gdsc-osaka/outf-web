0bbcf7ef126429bce0f397100e375d24c2a83fa8
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-02-13 00:52:27 +0900

    fix: add font-weight to a component's param

--- components/organisms/HeaderBasic/script.js
+++ components/organisms/HeaderBasic/script.js
@@ -2,6 +2,12 @@ import config from "@/assets/config"
 
 export default {
   name: 'HeaderBasic',
+  props: {
+    brandNameFontWeight: {
+      type: String,
+      default: 'font-weight-bold',
+    }
+  },
   data: () => ({
     brandName: config.brand.nameShort,
   }),
