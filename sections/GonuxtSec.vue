8a04292dd5d377d84e03c83d6803e8bc8c117aea
yuukki-nishino <yuukinishino@nishinoyuukinoMacBook-Pro.local>
2022-03-06 15:49:11 +0900

    add section title

--- sections/GonuxtSec.vue
+++ sections/GonuxtSec.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="my-6 py-6">
     <v-divider class="my-6"></v-divider>
+    <h1>Gonuxt Section</h1>
     <GonuxtSubSection1 />
     <GonuxtSubSection2 />
     <GonuxtSubSection3 />
