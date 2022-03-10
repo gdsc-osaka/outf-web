8bc822daf4a6c3116f4d43a72b7301abb0d4e822
tun0001 <65064150+tun0001@users.noreply.github.com>
2022-03-10 21:31:43 +0900

    delete subsection

--- sections/GonuxtSec.vue
+++ sections/GonuxtSec.vue
@@ -3,8 +3,6 @@
     <v-divider class="my-6"></v-divider>
     <h1>Gonuxt Section</h1>
     <GonuxtSubSection1 />
-    <GonuxtSubSection2 />
-    <GonuxtSubSection3 />
   </section>
 </template>
 <script scoped>
