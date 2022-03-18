3f6a489378dca5abc504111c1832f8a73f0380cb
Takato Yamamoto <takato13926@icloud.com>
2022-03-18 22:50:29 +0900

    made shinkanjoho

---
+++ sections/ShinkanjohoSec.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="my-6 py-6">
+    <v-divider class="my-6"></v-divider>
+    <h1>新歓情報</h1>
+    <ShinkanjohoIntroAST1 />
+    <ShinkanjohoIntroAST2 />
+    <ShinkanjohoIntroAST3 />
+  </section>
+</template>
+<script scoped>
+export default {
+  name: 'ShinkanjohoSec',
+}
+</script>
+<style scoped lang="scss"></style>
---
+++ sections/shinkanjoho/IntroAST2/script.js
@@ -0,0 +1,6 @@
+export default {
+  name: 'IntroAST2',
+  data: () => ({
+    imgSrc: require('@/assets/shinkanjoho/img/image2.png'),
+  }),
+}
