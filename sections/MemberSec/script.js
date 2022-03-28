193c2115b2e9367edee09f1e8654f2502ab25fcf
h-atsu <jhuim08@gmail.com>
2022-03-28 23:24:14 +0900

    fix member sec link

--- sections/MemberSec/script.js
+++ sections/MemberSec/script.js
@@ -15,10 +15,5 @@ export default {
       type: String,
       default: 'primary',
     },
-    link: {
-      type: String,
-      default:
-        'http://www.osaka-u-athlete-club.com/OUTF/%e9%83%a8%e5%86%85%e5%b0%82%e7%94%a8top/member/',
-    },
   },
 }
