a52cd2d94263be673c7919f1fe4c990fbb174344
Ryo Shirai <rbasket75jam@gmail.com>
2022-04-02 10:59:02 +0900

    initial commit

--- sections/MemberSec/script.js
+++ sections/MemberSec/script.js
@@ -3,7 +3,73 @@ import MemberCardB from '@/components/organisms/MemberCardB.vue'
 export default {
   components: { MemberCardB },
   data() {
-    return {}
+    return {
+      members: [
+        {
+          name: '阪大1',
+          txt: '開発担当',
+          imgSrc: require('@/assets/gonuxt/img/sample02.jpg'),
+          txt2: '初めまして，休日は本を読んだり，散歩をしたりします．',
+          imgSrc2: "require('@/assets/gonuxt/img/sample01.jpg')"
+        },
+        {
+          name: '阪大2',
+          txt: '開発担当',
+          imgSrc: require('@/assets/gonuxt/img/sample02.jpg'),
+          txt2: '初めまして，休日は本を読んだり，散歩をしたりします．',
+          imgSrc2: "require('@/assets/gonuxt/img/sample01.jpg')"
+        },
+        {
+          name: '阪大3',
+          txt: '開発担当',
+          imgSrc: require('@/assets/gonuxt/img/sample02.jpg'),
+          txt2: '初めまして，休日は本を読んだり，散歩をしたりします．',
+          imgSrc2: "require('@/assets/gonuxt/img/sample01.jpg')"
+        },
+        {
+          name: '阪大4',
+          txt: '開発担当',
+          imgSrc: require('@/assets/gonuxt/img/sample02.jpg'),
+          txt2: '初めまして，休日は本を読んだり，散歩をしたりします．',
+          imgSrc2: "require('@/assets/gonuxt/img/sample01.jpg')"
+        },
+        {
+          name: '阪大5',
+          txt: '開発担当',
+          imgSrc: require('@/assets/gonuxt/img/sample02.jpg'),
+          txt2: '初めまして，休日は本を読んだり，散歩をしたりします．',
+          imgSrc2: "require('@/assets/gonuxt/img/sample01.jpg')"
+        },
+        {
+          name: '阪大6',
+          txt: '開発担当',
+          imgSrc: require('@/assets/gonuxt/img/sample02.jpg'),
+          txt2: '初めまして，休日は本を読んだり，散歩をしたりします．',
+          imgSrc2: "require('@/assets/gonuxt/img/sample01.jpg')"
+        },
+        {
+          name: '阪大7',
+          txt: '開発担当',
+          imgSrc: require('@/assets/gonuxt/img/sample02.jpg'),
+          txt2: '初めまして，休日は本を読んだり，散歩をしたりします．',
+          imgSrc2: "require('@/assets/gonuxt/img/sample01.jpg')"
+        },
+        {
+          name: '阪大8',
+          txt: '開発担当',
+          imgSrc: require('@/assets/gonuxt/img/sample02.jpg'),
+          txt2: '初めまして，休日は本を読んだり，散歩をしたりします．',
+          imgSrc2: "require('@/assets/gonuxt/img/sample01.jpg')"
+        },
+        {
+          name: '阪大9',
+          txt: '開発担当',
+          imgSrc: require('@/assets/gonuxt/img/sample02.jpg'),
+          txt2: '初めまして，休日は本を読んだり，散歩をしたりします．',
+          imgSrc2: "require('@/assets/gonuxt/img/sample01.jpg')"
+        },
+      ],
+    }
   },
   props: {
     btnTxt: {
