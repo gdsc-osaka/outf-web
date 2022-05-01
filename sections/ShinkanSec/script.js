bf8a12665ded3ddcda3e7f4516c59e9e5e3c2e49
Takato Yamamoto <takato13926@icloud.com>
2022-05-02 08:30:56 +0900

    upate ShinkanSec

--- sections/ShinkanSec/script.js
+++ sections/ShinkanSec/script.js
@@ -7,23 +7,23 @@ export default {
       events: [
         {
           imgSrc: require('@/assets/shinkanjoho/Img/img1.jpg'),
-          date: '4月17日',
+          date: '5月2日',
           title: 'zoom新歓',
           detail:
             '部活動に関することはもちろん、気になる大学生活の質問などなど、どんなことでも陸上部員がお答えします！途中入退室自由です。是非参加してください！ (陸上部の紹介＋パート別相談＋学部別相談)',
-          dayandtime: '日時：4月17日 19:00~21:00',
+          dayandtime: '日時：5月2日 18:00~20:00',
           place: 'zoom参加はこちらから↓',
           zoomlink:
-            'https://zoom.us/j/92629817378?pwd=blcrMml2ZUozMno5YlVFdzRIZHdnQT09',
-          zoomID: 'ミーティングID: 926 2981 7378',
+            'https://zoom.us/j/95476314818?pwd=cEVmOGxiOUZ0V1hXdDNXRlpMZWJsUT09',
+          zoomID: 'ミーティングID: 954 7631 4818',
           zoompass: 'パスコード: Osaka123',
         },
         {
           imgSrc: require('@/assets/shinkanjoho/Img/img2.jpg'),
-          date: '4月10日',
+          date: '4月10日(終了)',
           title: 'サーオリ',
           detail:
-            '陸上部員の話を直接聞くことができます。対面でしか聞けないこともあるはず！？そこのあなた！是非陸上部のブースに立ち寄ってみては？',
+            '陸上部員の話を直接聞くことができます。対面でしか聞けないこともあるはず！？そこのあなた！是非陸上部のブースに立ち寄ってみては？（イベントは終了しました。お越しいただいた皆さん、ありがとうございました！）',
           dayandtime: '日時：4月10日',
           place: '場所：豊中キャンパス',
         },
@@ -44,7 +44,8 @@ export default {
           imgSrc: require('@/assets/shinkanjoho/Img/img4.jpg'),
           date: '活動日(月・火・水・金・土)',
           title: '練習見学会',
-          detail: '*新歓Twitter、InstagramのDMや、メールにて予めお問い合わせください！',
+          detail:
+            '*新歓Twitter、InstagramのDMや、メールにて予めお問い合わせください！',
           dayandtime: '活動日(月・火・水・金・土)',
           place: '場所：すいらん（吹田キャンパスグラウンド）',
         },
